3e9cffab327f644b573dbbc7c842529bdce1dbf4
Hasnain Fazili <gamedev.hasnain@gmail.com>
2025-03-12 16:33:41 +0500

    Worker Logic Implementation

---
+++ Assets/Scripts/WorkerAI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class WorkerAI : MonoBehaviour
+{
+    private NavMeshAgent workerAgent;
+    //TEMP For testing
+    public Transform RestAreaTransform;
+
+    public void AssignedToResource(Transform resource)
+    {
+        workerAgent.SetDestination(resource.position);
+        //Once worker reaches destination begin OR add to the production of resource
+    }
+
+    public void RemovedFromResource()
+    {
+        workerAgent.SetDestination(RestAreaTransform.position);
+    }
+}
